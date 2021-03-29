dca42290a039fdc1862d082be1d75ace66b105be
Polina-coder <lobovich.polina@gmail.com>
2021-03-30 01:35:55 +0300

    feat: add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-
+  let additionStr;
+  let result;
+  if(options.separator==undefined)
+  {
+  options.separator="+";
+  }
+  if(options.addition!==undefined)
+  {
+  str+=String(options.addition);
+  
+  if(String(options.additionSeparator)==undefined)
+  options.additionSeparator="|" ;
+  for(let i=0; i<options.additionRepeatTimes-1; i++)
+  {
+  str+=String(options.additionSeparator);
+  str+=String(options.addition);
+  }
+  
+  }
+  result=str;
+  for(let j=0; j<options.repeatTimes-1; j++)
+  {
+  result+=options.separator;
+  result+=str;
+  }
+  
+  return result;
   };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -4,5 +4,13 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
 
-      }
+            let depth = 1;
+            for(let i = 0; i < arr.length; i++) {
+              if(typeof arr[i] == 'object') {
+                let dep = this.calculateDepth(arr[i]);
+                if(dep > depth - 1) depth = dep + 1;
+              }
+            }
+            return depth;
+        }
 };
